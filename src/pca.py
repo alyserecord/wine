07b053107c8d3fe4cf86f8f6af5298392c1d80d8
alyserecord <alyserecord@gmail.com>
2019-07-02 16:41:16 -0600

    updating pca figure

--- src/pca.py
+++ src/pca.py
@@ -46,7 +46,7 @@ def scree_plot(pca):
     num_components = pca.n_components_
     ind = np.arange(num_components)
     vals = pca.explained_variance_ratio_
-    fig,ax = plt.subplots(1,1,figsize=(16,16))
+    fig,ax = plt.subplots(1,1,figsize=(10,10))
     ax.plot(ind, vals)
     ax.scatter(ind, vals, s=50)
     for i in range(num_components):
@@ -54,9 +54,9 @@ def scree_plot(pca):
                    (ind[i]+0.2, vals[i]+0.005), 
                    va="bottom", 
                    ha="center", 
-                   fontsize=12)
-    ax.set_xlabel("Principal Component", fontsize=20)
-    ax.set_ylabel("Variance Explained (%)", fontsize=20)
+                   fontsize=14)
+    ax.set_xlabel("Principal Component", fontsize=24)
+    ax.set_ylabel("Variance Explained (%)", fontsize=24)
     ax.set_title('Scree Plot for Principal Components', fontsize=28)
     plt.savefig('../figures/pca_scree_plot.jpg')
 
@@ -65,5 +65,5 @@ if __name__ == '__main__':
     pca, principal_components = create_pca(X,10)
     price = np.load('../data/50x50/sorted_price.npy')
     # plot_pca(principal_components,price)
-    # scree_plot(pca)
+    scree_plot(pca)
     add_pcs_to_df(pca,'../data/50x50/sorted_df.csv',7)
