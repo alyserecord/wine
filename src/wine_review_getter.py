e184bbfc37d4423de89ff1b933d3577c95a1a9c2
alyserecord <alyserecord@gmail.com>
2019-07-10 20:42:25 -0600

    changed source of URLs

--- src/wine_review_getter.py
+++ src/wine_review_getter.py
@@ -40,8 +40,8 @@ def get_all_text(soup,i):
 
 
 def scraper(wine_list):
-    f = csv.writer(open('../data/wine_descriptions2.csv', 'w'))
-    f.writerow(['url','name', 'size','alcohol', 'description','reviews'])
+    # f = csv.writer(open('../data/wine_descriptions2.csv', 'w'))
+    # f.writerow(['url','name', 'size','alcohol', 'description','reviews'])
     for i in wine_list:
         URL = '{}{}'.format('https://www.wine.com',i)
         r = requests.get(URL)
@@ -57,7 +57,11 @@ def scraper(wine_list):
 
 
 if __name__ == '__main__':
-    df = pd.read_csv('../data/scraped_wine_data.csv')
-    urls = ['/product/chateau-clerc-milon-15-liter-magnum-2005/434799','/product/royal-tokaji-late-harvest-500ml-2016/414521','/product/vietti-barolo-rocche-5-liter-2015/525412']
+    # df = pd.read_csv('../data/scraped_wine_data.csv')
+    # urls = ['/product/chateau-clerc-milon-15-liter-magnum-2005/434799','/product/royal-tokaji-late-harvest-500ml-2016/414521','/product/vietti-barolo-rocche-5-liter-2015/525412']
     # urls = df['url'][1:].tolist()
+
+    df = pd.read_csv('../data/cleaned_data.csv',index_col=0)
+    urls = df[df.description.isnull()]['url'].tolist()
+
     scraper(urls)
