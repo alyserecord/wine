69aa1d7044095ae24988ee48e5e3a6363a6ec31c
alyserecord <alyserecord@gmail.com>
2019-07-11 17:20:45 -0600

    fixing try except statement

--- src/wine_review_getter.py
+++ src/wine_review_getter.py
@@ -45,16 +45,16 @@ def scraper(wine_list):
     for i in wine_list:
         URL = '{}{}'.format('https://www.wine.com',i)
         try:
-		r = requests.get(URL)
-        	soup = BeautifulSoup(r.content, 'html5lib')
-        	try:
-           		wine = get_all_text(soup,i)
-            		f = csv.writer(open('../data/wine_descriptions.csv', 'a'))
-            		f.writerow(wine)
-        	except:
-            		pass
-	except:
-		pass
+            r = requests.get(URL)
+            soup = BeautifulSoup(r.content, 'html5lib')
+            try:
+                wine = get_all_text(soup,i)
+                f = csv.writer(open('../data/wine_descriptions3.csv', 'a'))
+                f.writerow(wine)
+            except:
+            	pass
+        except:
+            pass
         # time.sleep(10)
 
 
