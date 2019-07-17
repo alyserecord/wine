c82b8ebc83cab6d07b1fde1f915e48ae0e1a119b
alyserecord <alyserecord@gmail.com>
2019-07-17 17:20:58 -0600

    updated web app to display images

--- web_app/app.py
+++ web_app/app.py
@@ -1,14 +1,15 @@
 # Demonstrates Bootstrap version 3.3 Starter Template
 # available here: https://getbootstrap.com/docs/3.3/getting-started/#examples
 
-from flask import Flask, render_template,request
+from flask import Flask, render_template,request, send_from_directory
 import pandas as pd
+import os
 app = Flask(__name__)
 
 # home page
 @app.route('/', methods=['GET','POST'])
 def index():
-    df = pd.read_csv('../wines/data/50x50/sorted_df.csv')
+    df = pd.read_csv('../data/50x50/sorted_df.csv')
     return render_template('index.html', table = df)
 
 @app.route("/search_results", methods=['GET','POST'])
@@ -17,17 +18,27 @@ def search_results():
         selected_origin = request.form.get('origin')
         selected_varietal = request.form.get('varietal')
         selected_price = request.form.get('price')
-        df = pd.read_csv('../wines/data/50x50/sorted_df.csv')
+        df = pd.read_csv('../data/50x50/sorted_df.csv')
         selected = df[(df['origin']==selected_origin) & (df['varietal']==selected_varietal) & (df['price_bins']==selected_price)]
         try:
             subset = selected.sample(10)
         except:
             subset = selected
         if subset.shape[0] > 0:
-            names = ', '.join(i for i in subset['name'])
+            subset['name'] = subset['name'].astype(str) + '.jpg'
+            subset = subset['name'].tolist()
         else:
-            names = 'No wines found. Please search again.'
-    return render_template("search_results.html") + names
+            subset = []
+    return render_template("search_results.html",image_list = subset)
+
+
+MEDIA_FOLDER = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))), 'images')
+MEDIA_FOLDER = '../images/'
+@app.route('/images/<path:filename>')
+def download_file(filename):
+    return send_from_directory(MEDIA_FOLDER, filename)
+
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, threaded=True, debug=False)
