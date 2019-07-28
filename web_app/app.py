82201e74039ca4cc3a65993809c7865dbd9d29fb
alyserecord <alyserecord@gmail.com>
2019-07-28 11:15:24 -0600

    updates to flask up for background and dynamic dropdown

--- web_app/app.py
+++ web_app/app.py
@@ -1,7 +1,7 @@
 # Demonstrates Bootstrap version 3.3 Starter Template
 # available here: https://getbootstrap.com/docs/3.3/getting-started/#examples
 
-from flask import Flask, render_template,request, send_from_directory
+from flask import Flask, render_template,request, send_from_directory,jsonify
 import pandas as pd
 import os
 app = Flask(__name__)
@@ -9,10 +9,11 @@ import sys
 sys.path.insert(0, '../src')
 from recommender import CosineSimilarity
 
+df = pd.read_csv('../data/64x64/sorted_df.csv')
+
 # home page
 @app.route('/', methods=['GET','POST'])
 def index():
-    df = pd.read_csv('../data/50x50/sorted_df.csv')
     return render_template('index.html', table = df)
 
 @app.route("/search_results", methods=['GET','POST'])
@@ -21,7 +22,7 @@ def search_results():
         selected_origin = request.form.get('origin')
         selected_varietal = request.form.get('varietal')
         selected_price = request.form.get('price')
-        df = pd.read_csv('../data/50x50/sorted_df.csv')
+        df = pd.read_csv('../data/64x64/sorted_df.csv')
         selected = df[(df['origin']==selected_origin) & (df['varietal']==selected_varietal) & (df['price_bins']==selected_price)]
         try:
             subset = selected.sample(12)
@@ -30,6 +31,8 @@ def search_results():
         if subset.shape[0] > 0:
             subset['name'] = subset['name'].astype(str) + '.jpg'
             subset = subset['name'].tolist()
+            if len(subset)% 3 != 0:
+                subset = subset[:-(len(subset)%3)]
         else:
             subset = []
     return render_template("search_results.html",image_list = subset)
@@ -42,8 +45,7 @@ def download_file(filename):
     return send_from_directory(MEDIA_FOLDER, filename)
 
 def get_wines(selected_wine):
-    df = pd.read_csv('../data/50x50/sorted_df.csv')
-    nmf_topics = pd.read_csv('../data/50x50/nmf_topics.csv')    
+    nmf_topics = pd.read_csv('../data/64x64/nmf_topics.csv')    
 
     cs = CosineSimilarity(df,nmf_topics)
     cs.prep_sorted_data()
@@ -56,7 +58,7 @@ def get_wines(selected_wine):
 
 @app.route('/recomendations', methods=['GET','POST'])
 def recommendations():
-    df = pd.read_csv('../data/50x50/sorted_df.csv')
+
     selected_wine = request.args.get('type')
     selected_wine_att = []
     selected_wine_att.append(df[df['name']==selected_wine[:-4]]['varietal'].values[0])
@@ -78,6 +80,19 @@ def recommendations():
         lst.append(df[df['name']==wine]['description'].values[0])
         items.append(lst)
     return render_template('recommendations.html',img_name = selected_wine, selected_wine_att = selected_wine_att, recs = items)
+
+@app.route('/get_varietal')
+def get_varietal():
+    origin = request.args.get('origin')
+
+    sub_df = df[df['origin'] == origin]
+    varietal = sub_df['varietal'].unique()
+    print(varietal)
+    varietal = ['Select a varietal'] + sorted(varietal)
+    print (varietal)
+    data = [{'id':i} for i in varietal]
+    print(data)
+    return jsonify(data)
 
 
 
